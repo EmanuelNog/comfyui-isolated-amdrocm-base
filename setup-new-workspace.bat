8445101d1303c032beff0f140de83248df752a15
Emanuel Nogueira <miranteazi@gmail.com>
2026-07-12 21:40:01 -0300

    Create custom_nodes dir in setup if missing

--- setup-new-workspace.bat
+++ setup-new-workspace.bat
@@ -13,6 +13,10 @@ git remote add "%remote_name%" "%remote_url%"
 git remote set-url --push upstream no_push
 
 echo.
+echo Creating custom_nodes directory...
+if not exist custom_nodes mkdir custom_nodes
+
+echo.
 echo Creating venv...
 uv venv || exit /b
 
--- setup-new-workspace.sh
+++ setup-new-workspace.sh
@@ -14,6 +14,10 @@ git remote add "$remote_name" "$remote_url"
 git remote set-url --push upstream no_push
 
 echo ""
+echo "Creating custom_nodes directory..."
+mkdir -p custom_nodes
+
+echo ""
 echo "Creating venv..."
 uv venv
 
